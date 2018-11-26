3ec522a0df499334a34d8d305039d560c9570e56
xiaoniubei <15210690703@163.com>
2018-11-26 21:54:52 +0800

    koa-art-template 扩展时间方法

--- app.js
+++ app.js
@@ -29,7 +29,10 @@ app.use(static(__dirname+"/static"));
 render(app, {
     root: path.join(__dirname, 'views'),   // 视图的位置
     extname: '.html',  // 后缀名
-    debug: process.env.NODE_ENV !== 'production'  //是否开启调试模式
+    debug: process.env.NODE_ENV !== 'production',  //是否开启调试模式
+    dateFormat: dateFormat=function(value){
+        return sd.format(value, "YYYY-MM-DD HH:mm:ss")
+    }/**扩展模板里面的方法*/
 });
 //设置session提供的中间件
 app.keys = ['some secret hurr'];
