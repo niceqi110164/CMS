7bbf38bd16f8266ca28198a3983447e20c30badb
xiaoniubei <15210690703@163.com>
2018-11-27 12:44:59 +0800

    用户列表

--- routes/admin/manager.js
+++ routes/admin/manager.js
@@ -1,11 +1,19 @@
 /**Created by xiaoqi on 2018/11/23*/
 
-let router = require("koa-router")();
+let router = require("koa-router")(),
+    DB = require("../../model/db.js");
 
 router.get('/', async (ctx)=>{
-    await ctx.render("admin/manager/list",{
-        "title":"用户管理"
-    });
+    //获取用户数据
+
+    let findResult = await DB.find("userInfo",{});
+    //console.log(findResult);
+    if(findResult.length>0){
+        await ctx.render("admin/manager/list",{
+            "title":"用户管理",
+            "list":findResult
+        });
+    }
 });
 router.get('/add', async (ctx)=>{
     await ctx.render("admin/manager/add",{
